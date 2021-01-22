f15a1440d99fb3b2b7b71168634f0d587a566a38
xsist14 <jason.d.guest@gmail.com>
2021-01-21 18:41:04 -0800

    finished the logic

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data import MENU, resources
 
 
 def get_coffee_bill(menu_item):
-    return MENU[menu_item]["cost"]
+    return MENU[menu_item]
 
 
 def get_order():
@@ -16,59 +16,88 @@ def get_order():
     return coffee_order
 
 
-def get_payment(num_quarters, num_dimes, num_nickles, num_pennies):
+def get_payment():
+    print("Please insert coins.")
+    quarters = int(input("how many quarters?:"))
+    dimes = int(input("how many dimes?: "))
+    nickles = int(input("how many nickles?: "))
+    pennies = int(input("how many pennies?: "))
     total = \
-              (num_quarters * .25) \
-            + (num_dimes * .1) \
-            + (num_nickles * .05) \
-            + (num_pennies * .01)
+        (quarters * .25) \
+        + (dimes * .1) \
+        + (nickles * .05) \
+        + (pennies * .01)
     return total
 
 
-coffee_command = get_order()
+while True:
+    payment_received = False
+    your_order = ""
+    coffee_received = False
+    coffee_command = get_order()
 
-if coffee_command == 'report':
-    print(resources)
-elif coffee_command == 'off':
-    quit()
-elif coffee_command == "espresso":
-    print(MENU["espresso"])
-    total_cost = get_coffee_bill(coffee_command)
-elif coffee_command == "latte":
-    print(MENU["latte"])
-    total_cost = get_coffee_bill(coffee_command)
-elif coffee_command == "cappuccino":
-    print(MENU["cappuccino"])
-    total_cost = get_coffee_bill(coffee_command)
+    if coffee_command == 'off':
+        total_cost = 0
+        quit()
+    elif coffee_command == "report":
+        total_cost = 0
+        print(resources)
+    elif coffee_command == "espresso":
+        your_order = get_coffee_bill(coffee_command)
+        total_cost = your_order["cost"]
+    elif coffee_command == "latte":
+        your_order = get_coffee_bill(coffee_command)
+        total_cost = your_order["cost"]
 
-# TODO 1: check which coffee was selected
-print("Please insert coins.")
-quarters = int(input("how many quarters?:"))
-dimes = int(input("how many dimes?: "))
-nickles = int(input("how many nickles?: "))
-pennies = int(input("how many pennies?: "))
-total_paid = get_payment(quarters, dimes, nickles, pennies)
+    elif coffee_command == "cappuccino":
+        your_order = get_coffee_bill(coffee_command)
+        total_cost = your_order["cost"]
+    else:
+        total_cost = 0
 
+    missing_ingredients = 0
+    machine_water = resources['water']
+    coffee_options = ['espresso', "latte", "cappuccino"]
+    if coffee_command in coffee_options:
+        order_water = your_order['ingredients']['water']
+        if coffee_command != "espresso":
+            machine_milk = resources['milk']
+            order_milk = your_order['ingredients']['milk']
+            if machine_milk >= order_milk:
+                resources['milk'] -= order_milk
+            else:
+                print("Sorry there is not enough milk.")
+                missing_ingredients += 1
 
+        if machine_water >= order_water:
+            resources['water'] -= order_water
+        else:
+            print("Sorry there is not enough water.")
+            missing_ingredients += 1
+        machine_coffee = resources['coffee']
+        order_coffee = your_order['ingredients']['coffee']
+        if machine_coffee >= order_coffee:
+            resources['coffee'] -= order_coffee
+        else:
+            print("Sorry there is not enough coffee.")
+            missing_ingredients += 1
+        if missing_ingredients == 0:
 
-# TODO 9: get total cost
-# TODO 2: calculate how much money was paid for the coffee
-if total_paid > total_cost:
-    change = total_paid - total_cost
-    print(f"Here is ${change} in change.")
-elif total_paid < total_cost:
-    print("Sorry that's not enough money. Money refunded.")
-    # start over
+            if total_cost > 0:
+                total_paid = get_payment()
 
-# TODO 3: check for insufficiencies in payment
-# TODO 4: check for insufficiencies in ingredients
-# insufficient_water_message = "Sorry there is not enough water."
-# insufficient_coffee_message = "Sorry there is not enough coffee."
-# insufficient_milk_message = "Sorry there is not enough milk."
+                if total_paid > total_cost and missing_ingredients == 0:
+                    change = total_paid - total_cost
+                    print(f"Here is ${change} in change.")
+                    payment_received = True
+                    coffee_received = True
+                elif total_paid == total_cost and missing_ingredients == 0:
+                    payment_received = True
+                    coffee_received = True
+                elif total_paid < total_cost and missing_ingredients == 0:
+                    print("Sorry that's not enough money. Money refunded.")
+                    coffee_received = False
+            if coffee_received and missing_ingredients == 0:
+                print(f"Here is your {coffee_command} ☕️. Enjoy!")
 
-# TODO 5: give user back correct change
-
-# give_drink_message = f"Here is your {coffee_choice} ☕️. Enjoy!"
-
-# TODO 6: Turn off the Coffee Machine by entering off to the prompt.
-# TODO 7: Print report.
+    response = input("type 'y' to continue")
